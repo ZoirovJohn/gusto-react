4f58ad99c6f1a575ac256b46fbdf980516089129
Thomas <zoirovjohn@gmail.com>
2025-04-09 03:32:19 +0900

    feat: menu search buttons configurations

--- src/app/pages/all-food/grid.tsx
+++ src/app/pages/all-food/grid.tsx
@@ -1,41 +1,9 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
-import FoodCard from "../../components/cards/foodCard";
-import { ProductInquiry } from "../../../lib/types/product";
-import ProductService from "../../services/ProductService";
+import { useSelector } from "react-redux";
 import { retrieveProducts } from "./selector";
-import { setProducts } from "./slice";
+import FoodCard from "../../components/cards/foodCard";
 
 function Grid() {
-  const dispatch = useDispatch();
   const products = useSelector(retrieveProducts);
-  const [loading, setLoading] = useState(true);
-  const productService = new ProductService();
-
-  useEffect(() => {
-    // Fetch products when component mounts
-    const fetchProducts = async () => {
-      try {
-        setLoading(true);
-        // Define your product inquiry parameters
-        const productInquiry: ProductInquiry = {
-          page: 1,
-          limit: 12,
-          order: "newest", // or any default order you prefer
-          // Add other optional parameters as needed
-        };
-
-        const productData = await productService.getProducts(productInquiry);
-        dispatch(setProducts(productData));
-      } catch (error) {
-        console.error("Failed to fetch products:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchProducts();
-  }, [dispatch]);
 
   return (
     <div
@@ -45,9 +13,7 @@ function Grid() {
       aria-labelledby="pills-home-tab"
     >
       <div className="row">
-        {loading ? (
-          <div className="col-12 text-center">Loading products...</div>
-        ) : products && products.length > 0 ? (
+        {products && products.length > 0 ? (
           products.map((product) => (
             <FoodCard
               key={product._id}
