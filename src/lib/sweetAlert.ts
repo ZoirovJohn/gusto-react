ec7cfe13a8d91e29ffc8ae773ffe9950fa755f72
Thomas <zoirovjohn@gmail.com>
2025-04-26 08:19:21 +0900

    fix: modify sweetalert

--- src/lib/sweetAlert.ts
+++ src/lib/sweetAlert.ts
@@ -3,12 +3,29 @@ import Swal from "sweetalert2";
 import { Messages } from "./config";
 
 export const sweetErrorHandling = async (err: any) => {
-  const error = err.response?.data ?? err;
-  const message = error ?? Messages.error1;
+  let message = Messages.error1;
+
+  if (err?.response?.data?.message) {
+    message = err.response.data.message;
+  } else if (err?.response?.data) {
+    message =
+      typeof err.response.data === "string"
+        ? err.response.data
+        : JSON.stringify(err.response.data);
+  } else if (err?.message) {
+    message = err.message;
+  } else if (typeof err === "string") {
+    message = err;
+  } else if (typeof err === "object") {
+    message = JSON.stringify(err);
+  }
+
   await Swal.fire({
     icon: "error",
+    title: "Oops!",
     text: message,
     showConfirmButton: false,
+    timer: 2500,
   });
 };
 
