3481916878e074f0fd2e07ef17cfed7e67b460ab
Thomas Zair <zoirovtokhirjon@gmail.com>
2026-03-24 15:24:01 +0900

    Fix missing VITE_API_URL fallback

--- src/app/context/SocketContext.tsx
+++ src/app/context/SocketContext.tsx
@@ -1,16 +1,17 @@
 import React, { createContext } from "react";
 import { io, Socket } from "socket.io-client";
+import { serverApi } from "../../lib/config";
 
-const socket = io(import.meta.env.VITE_API_URL as string, {
+const socket = io(serverApi, {
   withCredentials: true,
 });
+
 export const SocketContext = createContext<Socket>(socket);
 
 interface SocketProviderProps {
   children: React.ReactNode;
 }
+
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
-  return (
-    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
-  );
+  return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
 };
